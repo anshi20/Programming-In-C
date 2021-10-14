1b8cd741eded459461096088b20a678e889735ad
Anshika Maheshwari <83770244+anshi20@users.noreply.github.com>
2021-10-14 18:54:45 +0530

    Update infix_to_prefix.c

--- infix_to_prefix.c
+++ infix_to_prefix.c
@@ -1,72 +1,168 @@
-#include<stdio.h>
-#include<ctype.h>
-#include<string.h>
-char stack[100];
-int top = -1;
-
-void push(char x)
-{
-    stack[++top] = x;
+        #include <stdio.h>
+        #include <math.h>
+        #include <string.h>
+        #include <stdlib.h>
+        #define MAX 20
+        void push(int);
+        char pop();
+        void infix_to_prefix();
+        int precedence(char);
+        char stack[20], infix[20], prefix[20];
+        int top = -1;
+        int main()
+        {
+                printf("\nINPUT THE INFIX EXPRESSION : ");
+                scanf("%s", infix);
+                infix_to_prefix();
+                return 0;
+        }
+        void push(int pos)
+        {
+                if (top == MAX - 1)
+                {
+                        printf("\nSTACK OVERFLOW\n");
+                }
+                else
+                {
+                        top++;
+                        stack[top] = infix[pos];
+                }
+        }
+        char pop()
+        {
+                char ch;
+                if (top < 0)
+                {
+                        printf("\n UNDERFLOW CONDITION\n");
+                        exit(0);
+                }
+                else
+                {
+                        ch = stack[top];
+                        stack[top] = '\0';
+                        top--;
+                        return (ch);
+                }
+                return 0;
+        }
+        void infix_to_prefix()
+        {
+                int i = 0, j = 0;
+                int m=0, n=0;    
+                n = strlen (prefix) - 1;  
+                while ( m < n)   
+                {    
+                  temp = prefix[n];  
+                  prefix[m] = prefix[n];  
+                  prefix[m] = temp;  
+                  m++;   
+                  n--;   
+                }  
+                infix[n]='\0';
+                while (infix[i] != '\0')
+                {
+                        if (infix[i] >= 'a' && infix[i] <= 'z')
+                        {
+                                prefix[j] = infix[i];
+                                j++;
+                                i++;
+                        }
+                else if (infix[i] == ')' || infix[i] == '}' || infix[i] == ']')
+                {
+                        push(i);
+                        i++;
+                }
+                else if (infix[i] == '(' || infix[i] == '{' || infix[i] == '[') 
+                {
+                        if (infix[i] == '(')
+                        {
+                                while (stack[top] != ')') /* pop till opening bracket is found */
+                               {
+                                        prefix[j] = pop();
+                                        j++;
+                                }
+                                pop();
+                                i++;
+                        }
+                        else if (infix[i] == '[')
+                        {
+                                while (stack[top] != ']') 
+                                {
+                                        prefix[j] = pop();
+                                        j++;
+                                }
+                                pop();
+                                i++;
+                        }
+                        else if (infix[i] == '{')
+                        {
+                                while (stack[top] != '}') 
+                                {
+                                       prefix[j] = pop();
+                                        j++;
+                                }
+                                pop();
+                                i++;
+                        }
+                }
+                else
+                {
+                        if (top == -1)
+                        {
+                                push(i);
+                                i++;
+                        }
+                        else if (precedence(infix[i]) < precedence(stack[top]))
+                        {
+                                prefix[j] = pop(); 
+                                j++;              
+                                while (precedence(stack[top]) > precedence(infix[i]))
+                               {
+                                        prefix[j] = pop(); /
+                                        j++;
+                                        if (top < 0)
+                                        {
+                                                break;
+                                        }
+                                }
+                                push(i);
+                                i++;
+                        }
+                        else if (precedence(infix[i]) >= precedence(stack[top]))
+                        {
+                                push(i);
+                                i++;
+                        }
+                }
+        }
+        while (top != -1)
+        {
+                prefix[j] = pop();
+                j++;
+        }
+        int k=0, l=0;    
+        l = strlen (prefix) - 1;  
+        while ( k < l)   
+        {    
+ 
+         temp = prefix[l];  
+         prefix[l] = prefix[k];  
+         prefix[k] = temp;  
+         k++;   
+         l--;   
+        }  
+        prefix[l] = '\0';
+        printf("EQUIVALENT PREFIX NOTATION : %s ", prefix);
 }
-
-char pop()
+int precedence(char alpha)
 {
-    if(top == -1)
-        return -1;
-    else
-        return stack[top--];
-}
-
-int priority(char x)
-{
-    if(x == '(')
+        if (alpha == '+' || alpha == '-')
+        {
+                return (1);
+        }
+        if (alpha == '*' || alpha == '/')
+        {
+                return (2);
+        }
         return 0;
-    if(x == '+' || x == '-')
-        return 1;
-    if(x == '*' || x == '/')
-        return 2;
-    return 0;
-}
-
-int main()
-{
-    char exp[100],temp;
-    char *e, x;
-    printf("Enter the expression : ");
-    scanf("%s",exp);
-    int len,j,i;
-    len=strlen(exp);
-    for(i=0,j=len-1;i<=j;i++,j--)
-    {
-      temp=str[i];
-      str[i]=str[j];
-      str[j]=temp;
-    }
-    printf("\n");
-    e = exp;
-    
-    while(*e != '\0')
-    {
-        if(isalnum(*e))
-            printf("%c ",*e);
-        else if(*e == '(')
-            push(*e);
-        else if(*e == ')')
-        {
-            while((x = pop()) != '(')
-                printf("%c ", x);
-        }
-        else
-        {
-            while(priority(stack[top]) >= priority(*e))
-                printf("%c ",pop());
-            push(*e);
-        }
-        e++;
-    }
-    
-    while(top != -1)
-    {
-        printf("%c ",pop());
-    }return 0;
 }
