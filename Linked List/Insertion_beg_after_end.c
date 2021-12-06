c5da48d4cae65ed82673bb1da66f2a8e3387d389
Anshika Maheshwari <83770244+anshi20@users.noreply.github.com>
2021-12-06 19:45:41 +0530

    Update Insertion_beg_after_end.c

--- Linked List/Insertion_beg_after_end.c
+++ Linked List/Insertion_beg_after_end.c
@@ -54,29 +54,45 @@ void display( struct node* ptr)
         ptr= ptr->next;
     }
 }
-/*
+
 struct node* DeleteFromBeg(struct node* head)
 {
-    
+    struct node* ptr=head;
+    head=head->next;
+    free(ptr);
+    return head;
 }
 struct node* DeleteFromEnd( struct node* head)
 {
-    
+    struct node* ptr = head;
+    while(ptr->next->next!=NULL)
+    {
+        ptr=ptr->next;
+    }
+    struct node* temp = ptr->next;
+    ptr->next=NULL;
+    free(temp);
+    return head;
 }
-*/
+
 int main()
 {
     struct node* head= NULL;
+    printf("\nNew element : 10 (Inserted in beginning)\n");
     head=insertAtBeg(head,10);
+    display(head);
+    printf("\nNew element : 20 (Inserted in beginning)\n");
     head=insertAtBeg(head,20);
+    display(head);
+    printf("\nNew element : 30 (Inserted in beginning)\n");
     head=insertAtBeg(head,30);
     display(head);
     printf("\n");
     head=insertAtEnd(head,40);
-    printf("\nNew element : 40\n");
+    printf("\nNew element : 40 (Inserted at end) \n");
     display(head);
     head=insertAtEnd(head,60);
-    printf("\nNew element : 60\n");
+    printf("\nNew element : 60 (Inserted at end)\n");
     display(head);
     printf("\n");
     head=insertInMiddle(head,20,50);
@@ -85,5 +101,22 @@ int main()
     head=insertInMiddle(head,60,70);
     printf("\nNew element : 70 to be inserted after 60\n");
     display(head);
+    printf("\n");
+    head = DeleteFromBeg(head);
+    printf("\nDeletion From Beginning!\n");
+    display(head);
+    printf("\n");
+    head = DeleteFromBeg(head);
+    printf("\nDeletion From Beginning!\n");
+    display(head);
+    printf("\n");
+    head = DeleteFromEnd(head);
+    printf("\nDeletion From End!\n");
+    display(head);
+    printf("\n");
+    head = DeleteFromEnd(head);
+    printf("\nDeletion From End!\n");
+    display(head);
+    printf("\n");
     return 0;
 }
